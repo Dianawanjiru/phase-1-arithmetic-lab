43c39ab4abe167111efa9440ebe842aabea77b91
Diana <diana.wanjiru@student.moringaschool.com>
2022-08-17 16:10:19 +0300

    completed assignmet

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2)
+console.log(multiply);
+
+
+//generates a random number whose integer is greater than 0
+
+let random = (Math.floor(Math.random() * 10) + 1);
+
+//mod integer
+let num3 = 24;
+let num4 = 5;
+let mod = (num3 % num4)
+console.log(mod);
+
+let a = 5;
+let b = 10;
+let c = 15;
+let z = 20;
+let max = (Math.max(a, b, c, z))
+
+
